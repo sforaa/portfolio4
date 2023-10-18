e4e6500ebfeddc76f287ceef03a7fae80c95b861
sforaa <146346892+sforaa@users.noreply.github.com>
2023-10-18 21:58:19 +0200

    Add files via upload

---
+++ assets/Work2View-1d92b143.js
@@ -0,0 +1,1 @@
+import{o as t,c as o,a as s,b as e,F as a}from"./index-0fb0e865.js";const n={class:"about"},r=e("h1",null,"verzamelde verzamelingen",-1),c=e("h2",null,"quelque part entre le nord et le sud.",-1),l=e("div",null," tekst ",-1),m={__name:"Work2View",setup(_){return(d,u)=>(t(),o("div",n,[r,c,l,s(a)]))}};export{m as default};
